8a5b9b3d4f56b13b32350ecad57cfc5db9524f1c
polarbird <jlm0808@126.com>
2020-03-20 17:48:22 +0800

    init project

---
+++ src/main/java/Main001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main001 {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        while (!"".equals(scanner.nextLine())){
+            String msg = scanner.nextLine();
+            System.out.println(msg);
+        }
+
+        System.out.println("hello");
+
+    }
+}
